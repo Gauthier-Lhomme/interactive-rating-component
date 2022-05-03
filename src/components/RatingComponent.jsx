d2adad0edd58caf47656f6aa43b1d11ced87b7af
Gauthier-Lhomme <lhomme.gaut@gmail.com>
2022-05-03 13:11:05 +0200

    Css RatingComponent

--- src/components/RatingComponent.jsx
+++ src/components/RatingComponent.jsx
@@ -1,25 +1,28 @@
 import React from "react";
 import iconStar from "../images/icon-star.svg";
+import { DivButton, Li, MainDiv, RatingSection, StarImg, Ul } from "../styled-components/RatingComponent";
 
 export default class RatingComponent extends React.Component {
   render() {
     return (
-      <div>
-        <img src={iconStar} alt="" />
-        <h1>How did we do ?</h1>
-        <p>
-          Please let us know how we did with your support request. All feedback
-          is appreciated to help us improve our offering !
-        </p>
-        <ul>
-          <li>1</li>
-          <li>2</li>
-          <li>3</li>
-          <li>4</li>
-          <li>5</li>
-        </ul>
-        <div>Submit</div>
-      </div>
+      <MainDiv>
+        <RatingSection>
+          <StarImg src={iconStar} alt="" />
+          <h1>How did we do ?</h1>
+          <p>
+            Please let us know how we did with your support request. All
+            feedback is appreciated to help us improve our offering !
+          </p>
+          <Ul>
+            <Li>1</Li>
+            <Li>2</Li>
+            <Li>3</Li>
+            <Li>4</Li>
+            <Li>5</Li>
+          </Ul>
+          <DivButton>Submit</DivButton>
+        </RatingSection>
+      </MainDiv>
     );
   }
 }
---
+++ src/styled-components/RatingComponent.jsx
@@ -0,0 +1,46 @@
+import styled from "styled-components";
+
+export const MainDiv = styled.div`
+background-color: hsl(216, 12%, 8%) ;
+padding: 20vh ;
+color: white ;
+`;
+
+export const RatingSection = styled.section`
+background-color: hsl(213, 19%, 18%) ;
+margin-left: 65vh ;
+margin-right: 65vh ;
+padding-top: 3vh ;
+padding-left: 2vh;
+height: 35vh ;
+border-radius: 25px ;
+`
+
+export const StarImg = styled.img`
+background-color: hsl(217, 12%, 63%) ;
+padding: 1vh ;
+border-radius: 100%;
+`
+
+export const Ul = styled.ul`
+display: flex ;
+list-style: none ;
+padding: 0;
+`
+export const Li = styled.li`
+padding: 1vh 2vh ;
+background-color: hsl(217, 12%, 63%) ;
+border-radius: 100%;
+margin-right: 3vh ;
+margin-left: 0.5vh ;
+`
+
+export const DivButton = styled.div`
+background-color: #cc8a0f ;
+text-align: center ;
+margin-right: 2vh ;
+font-size: 2vh ;
+border-radius: 25px ;
+padding : 1vh 0;
+font-weight: bold ;
+`
