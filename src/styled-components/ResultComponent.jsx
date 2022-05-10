2c3b0a6c6d70d591b6ff5f1d53a0b4f00632e7b0
Gauthier-Lhomme <lhomme.gaut@gmail.com>
2022-05-10 14:17:55 +0200

    change styled component verison

--- src/styled-components/ResultComponent.jsx
+++ src/styled-components/ResultComponent.jsx
@@ -1,39 +1,34 @@
 import styled from "styled-components";
 import { Device } from "../asset/device";
 
-
 export const MainDiv = styled.div`
   background-color: hsl(216, 12%, 8%);
   color: white;
-  padding-top: 26vh ;
-  padding-bottom: 34vh ;
-  @media ${Device.mobile}{
-margin: 0;
-padding:0 ;
-padding: 25.9vh 0vh;
-
+  padding-top: 26vh;
+  padding-bottom: 34vh;
+  @media ${Device.mobile} {
+    margin: 0;
+    padding: 0;
+    padding: 25.9vh 0vh;
   }
 `;
-
-
 
 export const ConfirmImg = styled.img`
   display: block;
   margin-left: auto;
   margin-right: auto;
-@media ${Device.mobile}{
-  display: block;
-  margin-left: auto;
-  margin-right: auto;  }
-`
-
+  @media ${Device.mobile} {
+    display: block;
+    margin-left: auto;
+    margin-right: auto;
+  }
+`;
 
 export const SuccessP = styled.p`
   color: white;
   text-align: center;
-  font-weight: bold ;
-  font-size: 22px ;
-
+  font-weight: bold;
+  font-size: 22px;
 `;
 
 export const ResultSection = styled.section`
@@ -42,8 +37,8 @@ export const ResultSection = styled.section`
   padding: 3vh;
   width: 40vh;
   border-radius: 25px;
-  @media ${Device.mobile}{
-    margin: 0 auto ;
+  @media ${Device.mobile} {
+    margin: 0 auto;
   }
 `;
 
@@ -51,20 +46,18 @@ export const NoteP = styled.p`
   color: #cc8a0f;
   background-color: #1214174b;
   margin: 0 auto;
-  margin-top: 2vh ;
-  text-align: center ;
+  margin-top: 2vh;
+  text-align: center;
   border-radius: 25px;
   font-weight: bold;
-  padding-top: 0.5vh ;
-  padding-bottom:0.5vh ;
-  @media ${Device.mobile}{
+  padding-top: 0.5vh;
+  padding-bottom: 0.5vh;
+  @media ${Device.mobile} {
     width: 58vw;
-
-
   }
 `;
 
 export const P = styled.p`
-text-align: center ;
-color: #8c8f94;
-`
+  text-align: center;
+  color: #8c8f94;
+`;
